04f959affee0f8ba871fa46d90e9c0bd85d94d50
Reenuay <reenuay777@gmail.com>
2021-08-29 21:19:14 +0400

    Remove unnecessary api code

--- Api.fs
+++ Api.fs
@@ -1,40 +1,6 @@
 module InsightClub.Creator.Bot.Api
 
-open Funogram.Api
-open Funogram.Telegram.Api
-open Funogram.Telegram.Bot
-open Utils
-open Db
 
-
-let onStart ctx =
-  option {
-    let! message = ctx.Update.Message
-    let! firstName = message.Chat.FirstName
-    let! lastName = message.Chat.LastName
-
-    $"Добро пожаловать, {firstName} {lastName}!\n"
-    |> sendMessage message.Chat.Id
-    |> api ctx.Config
-    |> Async.Ignore
-    |> Async.Start
-  } |> ignore
-
+// Stub
 let updateArrived ctx =
-  option {
-    let! message = ctx.Update.Message
-    let! username = message.Chat.Username
-
-    if Config.Bot.AllowedUsers.Contains(username) then
-      processCommands ctx [
-        cmd "/start" onStart
-        cmd "/restart" onStart
-      ]
-      |> ignore
-    else
-      "Нет доступа"
-      |> sendMessage message.Chat.Id
-      |> api ctx.Config
-      |> Async.Ignore
-      |> Async.Start
-  }   |> ignore
+  ()
