bc4cd9f0fb63f1025d31fe873a45629e2a2aaca0
Reenuay <reenuay777@gmail.com>
2021-08-30 02:20:46 +0400

    Refactor entry point, move config to it
    No need for separate Config module. Config belongs to root of the app,
    so loading it there, checking for config file existence, writing to
    console if it's not there and passing config to functions through
    parameters is the best. Also no need to use async blocks in Program
    module. All the code there is synchronous hence imperative style suits
    it better than half-functional, half-imperative. Code becomes much more
    readable and elegant.

--- Program.fs
+++ Program.fs
@@ -1,6 +1,8 @@
 module InsightClub.Creator.Bot.Program
 
+open System.IO
 open System.Net
+open FSharp.Configuration
 open Funogram.Api
 open Funogram.Types
 open Funogram.Telegram.Api
@@ -8,55 +10,66 @@ open Funogram.Telegram.Bot
 open Api
 
 
-let bot =
+type Config = YamlConfig<"Default.Config.yaml">
+
+let startBot (appConfig: Config) =
   // YamlConfig adds additional '/' character at the end of urls
   // So don't prepend apiPath with '/'
-  let apiPath = $"api/{Config.Token}"
+  let apiPath = $"api/{appConfig.Token}"
 
-  let webSocketEndpoint = Config.Server.Address.ToString() + apiPath
+  let webhookUrl =
+    appConfig.Server.Address.ToString() + apiPath
+
+  use listener = new HttpListener()
+  listener.Prefixes.Add(appConfig.Server.Listen)
+
+  let validate (req: HttpListenerRequest) =
+    req.Url.LocalPath = $"/{apiPath}"
+
+  let webhook =
+    { Listener = listener
+      ValidateRequest = validate }
 
   let botConfig =
     { defaultConfig with
-        Token = Config.Token }
+        Token = appConfig.Token
+        WebHook = Some webhook }
 
-  async
-    { let! hook =
-        setWebhookBase webSocketEndpoint None None None
-        |> api botConfig
+  let start _ =
+    printfn "Bot started! Listening to %s" appConfig.Server.Listen
+    startBot botConfig updateArrived None
+    |> Async.RunSynchronously
 
-      match hook with
-      | Ok _ ->
-        printfn "---------------Starting server---------------"
-        printfn "Server will listen at %s" Config.Server.ListenTo
+  let printError e =
+    printfn "Failed creating webhook:"
+    printfn "%A" e
 
-        use listener = new HttpListener()
-
-        listener
-          .Prefixes
-          .Add(Config.Server.ListenTo)
-
-        let validate (req: HttpListenerRequest) =
-          req.Url.LocalPath = $"/{apiPath}"
-
-        let webhook =
-          { Listener = listener
-            ValidateRequest = validate }
-
-        let botConfig =
-          { botConfig with
-              WebHook = Some webhook }
-
-        do! startBot botConfig updateArrived None
-
-      | Error e ->
-        printfn "---------------Can't set webhook---------------"
-        printfn "%A" e }
-
-[<EntryPoint>]
-let main _ =
   // Run synchronously to block the tread
   // Don't use Async.StartImmediate
   // or program will immediately shut after the launch
-  Async.RunSynchronously bot
+  setWebhookBase webhookUrl None None None
+  |> api botConfig
+  |> Async.RunSynchronously
+  |> Result.map start
+  |> Result.mapError printError
+  |> ignore
+
+[<EntryPoint>]
+let main _ =
+  let configFilePath =
+  #if DEBUG
+    "../../../Config.yaml"
+  #else
+    "Config.yaml"
+  #endif
+
+  let appConfig = Config()
+
+  if (File.Exists configFilePath) then
+    appConfig.Load(configFilePath)
+
+    startBot appConfig
+  else
+    printfn "Please, provide a Config.yaml file"
 
   0 // Return an integer exit code
