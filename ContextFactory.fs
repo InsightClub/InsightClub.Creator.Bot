c4293bb026086aee5d1ad5f1d4bd75a5baaaeb02
Reenuay <reenuay777@gmail.com>
2021-08-30 13:17:52 +0400

    Make connection string stub non-empty
    Npgsql.EntityFrameworkCore.PostgreSQL requires non-empty connection
    string to work. Without the string stub EFCore tools will not work.

--- ContextFactory.fs
+++ ContextFactory.fs
@@ -6,4 +6,6 @@ open Microsoft.EntityFrameworkCore.Design
 type internal ContextFactory() =
   interface IDesignTimeDbContextFactory<Context> with
     member _.CreateDbContext(_: string array) =
-      new Context("")
+      // Npgsql.EntityFrameworkCore.PostgreSQL requires
+      // non-empty connection string.
+      new Context("stub")
